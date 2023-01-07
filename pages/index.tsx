2c38bc6ddd61bd686ca306edf91e3d0b80f22dd5
kemmo47 <longkem123456@gmail.com>
2023-01-07 23:44:02 +0700

    create nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline text-red-500 mt-9">
+          hello worldsss
+        </h1>
+        <div className="bg-red-700"></div>
+
+      </div>
+    </>
+  );
+}
