25e013b0257be56df9d8b3e742de72a9d0891259
kemmo47 <longkem123456@gmail.com>
2023-01-08 10:53:53 +0700

    finished create nextapp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 export default function Home() {
-  return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline text-red-500 mt-9">
-          hello worldsss
-        </h1>
-        <div className="bg-red-700"></div>
-
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<h1 className="text-3xl font-bold underline text-red-500 mt-9">hello worlds</h1>
+				<div className="bg-red-700 text-white w-auto h-10">VietNam</div>
+			</div>
+		</>
+	)
 }
