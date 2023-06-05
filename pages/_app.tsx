8d63b5e58c993b0a533febaf1a485af4651d10eb
kemmo47 <longkem123456@gmail.com>
2023-06-05 16:13:37 +0700

    feat: add google search console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
 		<>
 			<Head>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="google-site-verification" content="roXUnA09_bunaG-lckoRo92P5dtokt1Q5Jrd8VmEOI4" />
 				<meta name="theme-color" content="#ffccdc" />
 			</Head>
 			<Component {...pageProps} />
