72f833c2a43b84b3db500da8b38860cef0de1515
kemmo47 <longkem123456@gmail.com>
2023-06-08 16:02:27 +0700

    feat: add OneSignal 5

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,20 +5,18 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<Script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" defer />
-				<Script
-					id="OneSignal"
-					dangerouslySetInnerHTML={{
-						__html: `
-              window.OneSignal = window.OneSignal || [];
-              OneSignal.push(function() {
-                OneSignal.init({
-                  appId: "0c6e02bd-ad9d-4288-82be-b6b320b145f9",
-                });
+				<Script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" defer></Script>
+
+				<Script strategy="beforeInteractive" id="OneSignal">
+					{`
+            window.OneSignal = window.OneSignal || [];
+            OneSignal.push(function() {
+              OneSignal.init({
+                appId: "0c6e02bd-ad9d-4288-82be-b6b320b145f9",
               });
-            `,
-					}}
-				/>
+            });
+        `}
+				</Script>
 			</Head>
 			<body>
 				<Main />
