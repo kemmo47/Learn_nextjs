22dc6b73bf3feae16247bd0cdabe13388e568ed6
kemmo47 <longkem123456@gmail.com>
2023-06-08 15:31:25 +0700

    feat: add OneSignal

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,31 @@
-import { Head, Html, Main, NextScript } from "next/document"
+import { Html, Main, NextScript } from "next/document"
+import Head from "next/head"
+import Script from "next/script"
 
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+      <Head>
+        <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" defer></script>
+        <Script
+          id="show-banner"
+          dangerouslySetInnerHTML={{
+            __html: `var OneSignal = window.OneSignal || [];
+            var initConfig = {
+                appId: "YOUR_APP_ID",
+                notifyButton: {
+                    enable: true
+                },
+            };
+            OneSignal.push(function () {
+                OneSignal.SERVICE_WORKER_PARAM = { scope: '/push/onesignal/' };
+                OneSignal.SERVICE_WORKER_PATH = 'push/onesignal/OneSignalSDKWorker.js'
+                OneSignal.SERVICE_WORKER_UPDATER_PATH = 'push/onesignal/OneSignalSDKWorker.js'
+                OneSignal.init(initConfig);
+            });`,
+          }}
+        />
+      </Head>
 			<body>
 				<Main />
 				<NextScript />
