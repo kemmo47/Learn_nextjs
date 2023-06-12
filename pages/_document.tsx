4ddd62e013bd54c20d534cfeb45fc2c181a7fc5e
kemmo47 <longkem123456@gmail.com>
2023-06-12 11:35:17 +0700

    feat: add GA4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,19 +5,9 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<Script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" defer></Script>
-
+				<Script async src="https://www.googletagmanager.com/gtag/js?id=G-CRFMR28XCQ"></Script>
 				<Script strategy="beforeInteractive" id="OneSignal">
-					{`
-            window.OneSignal = window.OneSignal || [];
-            OneSignal.initialize({
-              appId: '0c6e02bd-ad9d-4288-82be-b6b320b145f9',
-            }).then(function () {
-              OneSignal.push(['addListener', 'notificationPermission', function (permission) {
-                console.log('Permission:', permission);
-              }]);
-            });
-        `}
+					{`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-CRFMR28XCQ');`}
 				</Script>
 			</Head>
 			<body>
