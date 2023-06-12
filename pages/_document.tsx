5ca293962af6d825aa33ee2a4c8f86b978f223a8
kemmo47 <longkem123456@gmail.com>
2023-06-12 13:38:20 +0700

    feat: add firebase

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,25 @@ export default function Document() {
 			<body>
 				<Main />
 				<NextScript />
+				<Script type="module" strategy="afterInteractive" id="FireBase-console">
+					{`
+            import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+            import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
+
+            const firebaseConfig = {
+              apiKey: "AIzaSyAF9gUXamrzAo5yAQ8TTOt5j-OVXGeKsU0",
+              authDomain: "learn-nextjs-375b2.firebaseapp.com",
+              projectId: "learn-nextjs-375b2",
+              storageBucket: "learn-nextjs-375b2.appspot.com",
+              messagingSenderId: "246783941907",
+              appId: "1:246783941907:web:cc56ffae4c9d381019cee6",
+              measurementId: "G-JY9NN5F8YY"
+            };
+
+            const app = initializeApp(firebaseConfig);
+            const analytics = getAnalytics(app);
+          `}
+				</Script>
 			</body>
 		</Html>
 	)
