76fc8444001acfff0eed318220cd273bffb81d14
kemmo47 <longkem123456@gmail.com>
2023-08-25 15:23:27 +0700

    cant't import script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,8 @@
-import { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head>
-				<Script strategy="afterInteractive" id="show-banner" type="module">
-					{`
-            importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-            importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
-
-            const firebaseConfig = {
-              apiKey: "AIzaSyDld-b90iV0aRO3-DdsVimNPVwAjsHqayE",
-              authDomain: "laravel-engpush.firebaseapp.com",
-              projectId: "laravel-engpush",
-              storageBucket: "laravel-engpush.appspot.com",
-              messagingSenderId: "1083444428026",
-              appId: "1:1083444428026:web:4eb4431f2630086f281d43"
-            };
-
-            firebase.initializeApp(firebaseConfig);
-            const messaging = firebase.messaging();
-          `}
-				</Script>
-			</Head>
 			<body>
 				<Main />
 				<NextScript />
