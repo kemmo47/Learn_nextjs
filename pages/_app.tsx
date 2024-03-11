3772da59b086e9ed9ab9e8e754f9ac2ac878eff1
kemmo47 <longkem123456@gmail.com>
2024-03-11 18:53:07 +0700

    fix build 11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import favicon from './../public/favicon.ico'
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<link rel="icon" href={favicon.src} />
-				<meta name="theme-color" content="#CDEDFE" />
+				<meta name="theme-color" content="#ffccdc" />
 			</Head>
 			<Component {...pageProps} />
 		</>
 	)
 }
-
-export default MyApp
