617cbb189fcf5cfbc7bc34607cc25d9d6e423081
kemmo47 <longkem123456@gmail.com>
2024-03-11 18:48:43 +0700

    remove engpush 1

--- utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-	apiKey: 'AIzaSyAF9gUXamrzAo5yAQ8TTOt5j-OVXGeKsU0',
-	authDomain: 'learn-nextjs-375b2.firebaseapp.com',
-	projectId: 'learn-nextjs-375b2',
-	storageBucket: 'learn-nextjs-375b2.appspot.com',
-	messagingSenderId: '246783941907',
-	appId: '1:246783941907:web:cc56ffae4c9d381019cee6',
-	measurementId: 'G-JY9NN5F8YY',
-}
--- utils/firebase.ts
+++
@@ -1,8 +0,0 @@
-// firebase.ts
-import { getFirestore } from 'firebase/firestore'
-
-import FIREBASE_APP from './config' // Make sure this is properly configured
-
-const db = getFirestore(FIREBASE_APP)
-
-export default db
